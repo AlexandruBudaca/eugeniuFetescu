e2aa565147655909f741a925752a39f8b60d33af
Alex <alex.andrul10@gmail.com>
2021-03-05 15:20:41 +0000

    change layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Container, makeStyles } from "@material-ui/core"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import Seo from "../components/Seo"
 import pic from "../images/meAbout.jpg"
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Home from "../components/Home"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import Seo from "../components/Seo"
 const IndexPage = () => (
   <Layout>
